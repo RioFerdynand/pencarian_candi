88baf36f32fdd860e9664d61659638ad6fcad8b6
RioFerdynand <rioferdynand@mhs.mdp.ac.id>
2023-11-06 14:39:49 +0700

    p

---
+++ lib/screens/searchscreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  //TODO : 1. Dekalarasikan variabel yang dibutuhkan
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // TODO : 2. Buat appbar dengan judul Pencarian Candi
+        // TODO : 3. Buat body berupa Column
+        // TODO : 4. Buat TextField pencarian sebagai anak dari column
+        // TODO : 5. Buat ListView hasil pencarian sebagai anak dari column
+        ); // Scaffold
+  }
+}
