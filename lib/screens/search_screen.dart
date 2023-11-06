c9090c635ef31d70c14d3615e973a07596671e7e
RioFerdynand <rioferdynand@mhs.mdp.ac.id>
2023-11-06 15:20:09 +0700

    main

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pencarian_candi/models/candi_data.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,15 +9,84 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  //TODO : 1. Dekalarasikan variabel yang dibutuhkan
+  // TODO: 1. Deklarasikan variabel yanh dibutuhkan
+  List<Candi> _filteredCandis = [];
+  String _searchQuery = '';
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // TODO : 2. Buat appbar dengan judul Pencarian Candi
-      // TODO : 3. Buat body berupa Column
-      // TODO : 4. Buat TextField pencarian sebagai anak dari column
-      // TODO : 5. Buat ListView hasil pencarian sebagai anak dari column
-    ); // Scaffold
+      // TODO: 2. Buat appbar dengan judul Pencarian Candi
+      appBar: AppBar(
+        title: Text('Pencarian Candi'),
+      ),
+      // TODO: 3. Buat body berupa column
+      body: Column(
+        children: [
+          // TODO: 4. Buat TextField pencarian sebagai anak dari column
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.deepPurple[50],
+              ),
+              child: const TextField(
+                autofocus: false,
+                decoration: InputDecoration(
+                    hintText: 'Cari Candi...',
+                    prefixIcon: Icon(Icons.search),
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.deepPurple),
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 16, vertical: 12)),
+              ),
+            ),
+          ),
+          // TODO: 5. ListView hasil pencarian sebagai anak dari column
+          ListView.builder(
+              itemCount: _filteredCandis.length,
+              itemBuilder: (context, index) {
+                final candi = _filteredCandis[index];
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        width: 100,
+                        height: 100,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            candi.imageAsset,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              candi.name,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              })
+        ],
+      ),
+    );
   }
 }
